5d4d1afec80cd6bc070ca0b168267cf39f82ab6f
SvetlanaS <swetl.skorova@yandex.ru>
2021-05-11 22:58:19 +0300

    homeWork_6

---
+++ js/ProductComp_6.js
@@ -0,0 +1,64 @@
+//в компонентах данные хранятся в поле data, но здесь это не объект, а функция, возвращающая объект
+//template служит невидимой обёрткой и сам в результатах отрисовки не появляется
+//v-show: элемент всегда присутствует в DOM, и только CSS-свойство переключается в зависимости от условия
+//v-if: используется для рендеринга блока по условию. Блок будет отображаться только в том случае, если выражение директивы возвращает значение, приводимое к true
+//v-for: для отрисовки списка элементов на основе массива данных. У директивы v-for особый синтаксис записи: item in items, где items — исходный массив, а item — ссылка на текущий элемент массива
+//$emit генерация события в дочернем компоненте и обработка в родительском
+
+Vue.component('products', {
+    data() {
+        return {
+            catalogUrl: '/catalogData.json',
+            products: [],
+            filtered: [],
+            imgCatalog: 'https://via.placeholder.com/200x150',
+        }
+    },
+    methods: {
+        //Регулярка, поиск товаров
+        filter(value) {
+            let regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+    mounted() {
+        this.$parent.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+    },
+    template: `
+        <div class="products">
+            <product ref="refref" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+        </div>
+    `
+});
+Vue.component('product', {
+    props: ['product', 'img'],
+    data() {
+        return {
+            /**
+             * Создали ссылку на API нашей корзины. Т.к. все компоненты у нас регистрируются в корневом экземпляре Vue,
+             * то мы легко можем получить доступ к ним используя свойство $root.
+             * $parent можно использовать для доступа к родительскому экземпляру из дочернего.
+             */
+            cartAPI: this.$root.$refs.cart,
+        };
+    },
+
+    template: `
+    <div class="product-item">
+                <img :src="img" alt="Some img">
+                <div class="desc">
+                    <h3>{{product.product_name}}</h3>
+                    <p>{{product.price}}₽</p>
+                    <button class="buy-btn" @click="cartAPI.addProduct(product)">Купить</button>
+<!-- 1                    <button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>-->
+<!-- 2                    <button class="buy-btn" @click="$parent.$parent.$refs.cart.addProduct(product)">Купить</button>-->
+                </div>
+            </div>
+    `
+});
